755707af8628afc9071419b8688af09a879f723f
yurik703 <yurik.uschen@gmail.com>
2016-03-02 11:50:17 +0300

    Fixed alert service

--- Notes.Core/Interfaces/IAlertsService.cs
+++ Notes.Core/Interfaces/IAlertsService.cs
@@ -5,5 +5,7 @@ namespace Notes.Core.Interfaces
     public interface IAlertsService
     {
         Task<bool> ShowAlert(string title, string message);
+
+        Task<bool> ShowAlert(string title, string message, string positive, string negative);
     }
 }
--- Notes/Services/AlertService.cs
+++ Notes/Services/AlertService.cs
@@ -37,5 +37,39 @@ namespace Notes.Services
 
             return tcs.Task;
         }
+
+        public Task<bool> ShowAlert(string title, string message, string positive, string negative)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            var top = Mvx.Resolve<IMvxAndroidCurrentTopActivity>();
+            var act = top.Activity;
+            if (act == null)
+            {
+                // this can happen during transitions
+                // - you need to be sure that this won't happen for your code
+                throw new MvxException("Cannot get current top activity");
+            }
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(act);
+            alert.SetTitle(title);
+            alert.SetMessage(message);
+            alert.SetPositiveButton(positive, (sender, args) =>
+            {
+                tcs.SetResult(true);
+            });
+
+            alert.SetNegativeButton(negative, (sender, args) =>
+            {
+                tcs.SetResult(false);
+            });
+
+            Application.SynchronizationContext.Post(_ =>
+            {
+                alert.Show();
+            }, null);
+
+            return tcs.Task;
+        }
     }
 }
